02a09b8bec4ccdce15c3f1b227f4a322fa57c1a3
lucaspfeliciano <lucaspfeliciano@hotmail.com>
2020-05-24 22:24:20 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
